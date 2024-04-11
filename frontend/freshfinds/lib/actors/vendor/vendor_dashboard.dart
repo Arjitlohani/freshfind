fd6f7a2577bf2b2313111b3cd10f8b44874742ae
Arjit <arjitloh223@gmail.com>
2024-04-11 19:50:29 +0545

    Add debugShowCheckedModeBanner: false to MaterialApp in main.dart Import path_provider_foundation in GeneratedPluginRegistrant.swift Add fl_chart and file_picker dependencies in pubspec.yaml Update assets section in pubspec.yaml Import path_provider in pubspec.yaml

--- frontend/freshfinds/lib/actors/vendor/vendor_dashboard.dart
+++ frontend/freshfinds/lib/actors/vendor/vendor_dashboard.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:freshfinds/actors/vendor/productmmt.dart';
+
+import 'product_display.dart';
 
 void main() {
-  runApp(VendorDashboard());
+  runApp(const VendorDashboard());
 }
 
 class VendorDashboard extends StatelessWidget {
+  const VendorDashboard({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vendor Dashboard',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: VendorDashboardScreen(),
-    );
+    return const VendorDashboardScreen();
   }
 }
 
 class VendorDashboardScreen extends StatefulWidget {
+  const VendorDashboardScreen({super.key});
+
   @override
   _VendorDashboardScreenState createState() => _VendorDashboardScreenState();
 }
@@ -29,11 +31,20 @@ class _VendorDashboardScreenState extends State<VendorDashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Vendor Dashboard',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Color.fromARGB(255, 54, 99, 56),
+        backgroundColor: const Color.fromARGB(255, 54, 99, 56),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            onPressed: () => _logout(context),
+          ),
+        ],
       ),
       drawer: VendorDrawer(onTap: (index) {
         setState(() {
@@ -48,23 +59,31 @@ class _VendorDashboardScreenState extends State<VendorDashboardScreen> {
   Widget _buildBody(int index) {
     switch (index) {
       case 0:
-        return VendorHomeScreen();
+        return const VendorHomeScreen();
       case 1:
-        return Container(); // Placeholder for product management
+        return ProductManagementScreen(); // Placeholder for product management
       case 2:
-        return Container(); // Placeholder for order management
+        return ProductDisplay(); // Placeholder for order management
       case 3:
         return Container(); // Placeholder for user management
       default:
         return Container(); // Placeholder
     }
   }
+
+  // Function to handle logout
+  void _logout(BuildContext context) {
+    // Perform any necessary logout tasks here
+    // For example, clearing authentication tokens or session data
+    // Navigate back to the login screen
+    Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+  }
 }
 
 class VendorDrawer extends StatelessWidget {
   final Function(int) onTap;
 
-  const VendorDrawer({required this.onTap});
+  const VendorDrawer({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +91,7 @@ class VendorDrawer extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          DrawerHeader(
+          const DrawerHeader(
             decoration: BoxDecoration(
               color: Color.fromARGB(255, 54, 99, 56),
             ),
@@ -85,19 +104,19 @@ class VendorDrawer extends StatelessWidget {
             ),
           ),
           ListTile(
-            title: Text('Home'),
+            title: const Text('Home'),
             onTap: () => onTap(0),
           ),
           ListTile(
-            title: Text('Product Management'),
+            title: const Text('Product Management'),
             onTap: () => onTap(1),
           ),
           ListTile(
-            title: Text('Order Management'),
+            title: const Text('Order Management'),
             onTap: () => onTap(2),
           ),
           ListTile(
-            title: Text('User Management'),
+            title: const Text('User Management'),
             onTap: () => onTap(3),
           ),
         ],
@@ -107,6 +126,8 @@ class VendorDrawer extends StatelessWidget {
 }
 
 class VendorHomeScreen extends StatelessWidget {
+  const VendorHomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -121,7 +142,7 @@ class VendorHomeScreen extends StatelessWidget {
               Expanded(child: _buildContainer('Active Products', '50')),
             ],
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -129,6 +150,124 @@ class VendorHomeScreen extends StatelessWidget {
               Expanded(child: _buildContainer('Completed Orders', '20')),
             ],
           ),
+          const SizedBox(height: 20),
+          // Pie Chart for Sales
+          SizedBox(
+            height: 200,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Card(
+              child: PieChart(
+                PieChartData(
+                  sections: [
+                    PieChartSectionData(
+                      value: 30,
+                      color: Colors.blue,
+                      title: 'Fruits',
+                      radius: 50,
+                    ),
+                    PieChartSectionData(
+                      value: 40,
+                      color: Colors.green,
+                      title: 'Vegetables',
+                      radius: 50,
+                    ),
+                    PieChartSectionData(
+                      value: 20,
+                      color: Colors.orange,
+                      title: 'Dairy',
+                      radius: 50,
+                    ),
+                    PieChartSectionData(
+                      value: 10,
+                      color: Colors.red,
+                      title: 'Beverages',
+                      radius: 50,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          // Bar Graph for Income
+          // Container(
+          //   height: 200,
+          //   width: MediaQuery.of(context).size.width * 0.8,
+          //   child: Card(
+          //     child: BarChart(
+          //       BarChartData(
+          //         alignment: BarChartAlignment.center,
+          //         groupsSpace: 20,
+          //         barTouchData: BarTouchData(enabled: false),
+          //         titlesData: FlTitlesData(
+          //           show: true,
+          //           leftTitles: AxisTitles(
+          //             show: true,
+          //             getTextStyles: (value) => const TextStyle(
+          //               color: Colors.black,
+          //               fontWeight: FontWeight.bold,
+          //               fontSize: 14,
+          //             ),
+          //             margin: 8,
+          //             reservedSize: 32,
+          //             getTitles: (value) {
+          //               switch (value.toInt()) {
+          //                 case 0:
+          //                   return '0';
+          //                 case 2:
+          //                   return '20';
+          //                 case 4:
+          //                   return '40';
+          //                 default:
+          //                   return '';
+          //               }
+          //             },
+          //           ),
+          //           bottomTitles: AxisTitles(
+          //             show: true,
+          //             getTextStyles: (value) => const TextStyle(
+          //               color: Colors.black,
+          //               fontWeight: FontWeight.bold,
+          //               fontSize: 14,
+          //             ),
+          //             margin: 8,
+          //             reservedSize: 32,
+          //             getTitles: (value) {
+          //               switch (value.toInt()) {
+          //                 case 0:
+          //                   return 'A';
+          //                 case 1:
+          //                   return 'B';
+          //                 case 2:
+          //                   return 'C';
+          //                 case 3:
+          //                   return 'D';
+          //                 default:
+          //                   return '';
+          //               }
+          //             },
+          //           ),
+          //         ),
+          //         borderData: FlBorderData(show: false),
+          //         barGroups: [
+          //           BarChartGroupData(
+          //             x: 0,
+          //             barsSpace: 20,
+          //             barRods: [
+          //               BarChartRodData(toY: 8, color: Colors.blue),
+          //               BarChartRodData(toY: 10, color: Colors.green),
+          //               BarChartRodData(toY: 15, color: Colors.orange),
+          //               BarChartRodData(toYBarChartRodData(
+          // y: 7, // the y value
+          // colors: [Colors.red], // the color of the bar
+          // ): 7, color: Colors.red),
+          //             ],
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
@@ -136,10 +275,10 @@ class VendorHomeScreen extends StatelessWidget {
 
   Widget _buildContainer(String title, String value) {
     return Container(
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
       decoration: BoxDecoration(
-        color: Color.fromARGB(255, 54, 99, 56),
+        color: const Color.fromARGB(255, 54, 99, 56),
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
@@ -147,12 +286,13 @@ class VendorHomeScreen extends StatelessWidget {
         children: [
           Text(
             title,
-            style: TextStyle(color: Colors.white),
+            style: const TextStyle(color: Colors.white),
           ),
-          SizedBox(height: 5),
+          const SizedBox(height: 5),
           Text(
             value,
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold),
           ),
         ],
       ),
