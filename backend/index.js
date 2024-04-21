440aab11b09bc702f883a1b0a6554a4ab81e901e
Arjit <arjitloh223@gmail.com>
2024-04-22 01:38:29 +0545

    Update Twilio client initialization and change server IP address

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Initialize Twilio client
 
 
 // Temporary storage for OTP
@@ -455,7 +454,7 @@ app.get('/vendors', (req, res) => {
     });
 });
 
-const baseURL = 'http://192.168.1.113:3000';
+const baseURL = 'http://192.168.1.6:3000';
 app.get('/products/vendor/:vendorId', (req, res) => {
     const vendorId = req.params.vendorId;
     const query = `
